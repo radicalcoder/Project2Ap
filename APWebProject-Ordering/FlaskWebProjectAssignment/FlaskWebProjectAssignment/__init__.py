49558a58409854b9e8640b5e9f9927846229ed8f
AyushmanGhosh <ayushmanghose99@gmail.com>
2019-11-18 14:17:50 +0530

    Config changes to work on oy 3.6

--- APWebProject-Ordering/FlaskWebProjectAssignment/FlaskWebProjectAssignment/__init__.py
+++ APWebProject-Ordering/FlaskWebProjectAssignment/FlaskWebProjectAssignment/__init__.py
@@ -3,7 +3,7 @@ The flask application package.
 """
 
 from flask import Flask
-from config import Config, Configorder
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -11,28 +11,19 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-ord = Flask(__name__)
 app.config.from_object(Config)
-ord.config.from_object(Configorder)
 db = SQLAlchemy(app)
-dborder = SQLAlchemy(ord)
-#migrate = Migrate(app, db)
 login = LoginManager(app)
+
+#migrate = Migrate(app, db)
+
 
 
 @app.before_first_request
-def create_tables():
-
-    from FlaskWebProjectAssignment.models import User, Order
+def create_tables():  
+    from FlaskWebProjectAssignment.models import Order, User
     #Test code to delete old db   
     #db.drop_all()    
     db.create_all() 
-    admin = User(username='susan', email='susan@example.com')
-    p = Order(id = '1')
-    db.session.add(admin)
-    db.session.add(p)
-    db.session.commit
-   
 
 from FlaskWebProjectAssignment import views, models
-
